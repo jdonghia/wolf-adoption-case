3f1f9c1bb9cb1a2c0bdb1c4563bdf3d892d33a36
João <joaondd@gmail.com>
2021-06-01 10:43:12 -0300

    Creating onscroll header event function

---
+++ app.js
@@ -0,0 +1,22 @@
+(function() {
+        window.onscroll = () => {hideHeader()};
+        
+        function hideHeader() {
+
+            const $headerLink = document.querySelectorAll(".header__link");
+            const $headerLogo = document.querySelector(".header__logo");
+        
+            if(window.scrollY > 200) {
+                $headerLogo.style.height = "2.5rem";
+                $headerLogo.style.marginLeft = ".6rem";
+                $headerLink.forEach(v => v.style.fontSize = "1rem")          
+            } else {
+                $headerLogo.style.height = "6rem";
+                $headerLink.forEach(v => v.style.fontSize = "1.8rem")  
+                $headerLogo.style.marginLeft = "0";
+            }
+        }
+
+
+})();
+
