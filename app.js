3f36820ac0d4f07f62aea97bc74add4a43187400
João <joaondd@gmail.com>
2021-06-12 15:20:47 -0300

    creating getAPI and insertIndexWolf function

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 (function () {
   window.onload = () => {
-    window.onscroll = () => hideHeader();
+    getAPI();
 
+    function getAPI() {
+      const http = new XMLHttpRequest();
+      http.open("GET", "https://lobinhos.herokuapp.com/wolves/")
+      http.addEventListener("load", () => {
+        insertIndexWolf(JSON.parse(http.responseText));
+      });
+      http.send()
+    }
+
+    window.onscroll = () => hideHeader();
     function hideHeader() {
       const $headerLink = document.querySelectorAll(".header__link");
       const $headerLogo = document.querySelector(".header__logo");
@@ -16,5 +26,17 @@
         $headerLogo.style.marginLeft = "0";
       }
     }
+
+  
+    function insertIndexWolf(arr) {
+        const $wolfImg = document.querySelectorAll(".wolf__img");
+        const $wolfName = document.querySelectorAll(".wolf__name");
+        const $wolfAge = document.querySelectorAll(".wolf__age");
+        $wolfImg.forEach((v, i) => v.src = arr[i].link_image);
+        $wolfName.forEach((v, i) => v.textContent = arr[i].name);
+        $wolfAge.forEach((v, i) => v.textContent = arr[i].age);
+    }
+
+
   };
 })();
